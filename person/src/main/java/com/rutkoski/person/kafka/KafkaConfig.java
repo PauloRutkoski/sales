0f4faa3253e25b91c871833b04e542650fc81794
PauloRutkoski <paulo.rtkoski@gmail.com>
2022-02-02 21:03:06 -0300

    Last ajusts

--- person/src/main/java/com/rutkoski/person/kafka/KafkaConfig.java
+++ person/src/main/java/com/rutkoski/person/kafka/KafkaConfig.java
@@ -31,6 +31,7 @@ public class KafkaConfig {
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
+        System.out.println(host);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
